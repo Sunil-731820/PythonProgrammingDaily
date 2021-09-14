bae1a18f84fd8fbe374029f634e328387dd3f577
sunil <sunilkumargupta1170@gmail.com>
2021-09-15 00:47:41 +0530

    after finding the index of left side of maximum element

--- PeakElement.py
+++ PeakElement.py
@@ -7,8 +7,13 @@ def maximum_value():
     for j in range(size):
         print(arr[j],end=' ')
     max_value = arr[0]
+    index =0 
     for k in range(1,size):
         if (arr[k]>max_value):
             max_value = arr[k]
+            index+=1 
     print("\nthe maximum element is  ",max_value)
+    print("the index of the ,maximum value is ",index)
+    print("the index of the left index will be")
+    print(index-1)
 maximum_value()
